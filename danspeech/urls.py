08b324d583747c80edc96aa47dea13ce73d95156
arpelarpe <rasmus.arpe@gmail.com>
2019-06-07 10:58:39 +0200

    Demo template

--- danspeech/urls.py
+++ danspeech/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-
 from . import views
-
+from django.urls import include, path
 urlpatterns = [
+    path('demo/', include('demo.urls')),
     path('', views.index, name='index'),
     path('requirements/', views.requirements, name="requiremnets"),
     path('save/', views.save_audio, name='save'),
